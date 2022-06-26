f309c736e31fe6260c1d608a944bebe8bcb65739
seif <sifelkateb@gmail.com>
2022-06-26 15:32:11 +0200

    update routes/controllers

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -2,7 +2,38 @@ const getAllTasks=(req,res)=>{
     res.send('all tasks');
 }
 
+const addTask=(req,res)=>{
+    const {id}=req.body;
+    console.log(id);
+    res.json({id,Completed:'true'});
+
+}
+
+const getTask=(req,res)=>{
+    const {id}=req.params;
+    res.json({
+        id,
+        success:'true'
+    })
+}
+
+const deleteTask=(req,res)=>{
+    res.send('deleted the task')
+}
+
+const updateTask=(req,res)=>{
+    const {id}=req.params;
+    res.json({
+        id,
+        success:'true'
+    })
+}
+
 
 module.exports={
-    getAllTasks
+    getAllTasks,
+    getTask,
+    addTask,
+    deleteTask,
+    updateTask
 }
--- routes/tasks.js
+++ routes/tasks.js
@@ -2,14 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 
-const {getAllTasks}= require('../controllers/tasks');
+const {getAllTasks,addTask,getTask,deleteTask,updateTask}= require('../controllers/tasks');
 
 
  
 
+router.route('/').get(getAllTasks).post(addTask);
 
-
-router.route('/').get(getAllTasks);
+router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask);
 
 
 
