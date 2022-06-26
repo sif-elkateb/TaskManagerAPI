f97e4e7274a1b4d897cadb58c8b523274d34f313
seif <sifelkateb@gmail.com>
2022-06-26 13:38:58 +0200

    setup project structure and setup middleware functions

---
+++ controllers/tasks.js
@@ -0,0 +1,8 @@
+const getAllTasks=(req,res)=>{
+    res.send('all tasks');
+}
+
+
+module.exports={
+    getAllTasks
+}
--- server.js
+++ server.js
@@ -2,11 +2,15 @@
 start 
 initialize global variables
 */
-const express =require('express');
+const express = require("express");
 
-const app=express();
+const morgan = require("morgan");
 
-const port =3000;
+const app = express();
+
+const tasksRouter = require("./routes/tasks");
+
+const port = 3000;
 
 /*
 end intialize global variables
@@ -21,12 +25,31 @@ app.patch('/api/v1/tasks/:id) update the the specific task
 app.delete('/api/v1/tasks/:id') delete the specific task
 */
 
-app.get('/hello',(req,res)=>{
-    res.send('<h1>welcome to our task manager app</h1>');
-})
+/*
+start setting up middleware
+*/
 
-app.listen(port,()=>{
-    console.log(`started listening at port ${port}`);
-})
+app.use(morgan("tiny"));
 
+app.use(express.static('public'));
 
+app.use(express.json());
+
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+/*
+end setting up middleware 
+*/
+
+app.get("/hello", (req, res) => {
+  res.send("<h1>welcome to our task manager app</h1>");
+});
+
+app.use("/api/v1/tasks", tasksRouter);
+
+app.listen(port, () => {
+  console.log(`started listening at port ${port}`);
+});
